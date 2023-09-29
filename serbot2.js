1257f88837956a7e9c853a970e22967c1ef4493b
skid <112231743+Skidy89@users.noreply.github.com>
2023-09-29 07:38:26 -0600

    Add files via upload

--- serbot2.js
+++ serbot2.js
@@ -101,15 +101,13 @@ const _0x450013=_0x23d5;function _0x22f3(){const _0x6ac192=['9nCeAOg','base64','
    }
    const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode 
    console.log(code)
+   conn.logger.warn('\n' + lastDisconnect)
    if (connection === 'close') {
    if (code == 405) { 
    fs.unlinkSync("./jadibot/" + id + "/creds.json") 
    return skmod.sendMessage(m.chat, {text : "*❗ Por favor, reenvia el comando*"}, { quoted: m }) 
    }
-   if (code == 515) {
-   jadibots()
-   conn.logger.warn('error al stremear (reinicio)')
-   }
+   
    if (code === DisconnectReason.badSession) { 
    skmod.sendMessage(m.chat, {text : "*❗ La sesión actual es inválida, Tendras que iniciar sesion de nuevo."}, { quoted: m }) 
    fs.rm("./jadibot/" + id, { recursive: true }) 
@@ -125,7 +123,8 @@ const _0x450013=_0x23d5;function _0x22f3(){const _0x6ac192=['9nCeAOg','base64','
    skmod.sendMessage(m.chat, {text : "*❗ La sesión actual se cerró, Si desea volver a conectarse tendra que iniciar sesion de nuevo*"}, { quoted: m }) 
    } else if (code === DisconnectReason.restartRequired) { 
    skmod.sendMessage(m.chat, {text : "*❗ Reinicio requerido, se intentara reconectar automáticamente...*"}, { quoted: m }) 
-   await reloadHandler(true).catch(console.error)
+   await jadibots()
+   await reloadHandler().catch(console.error)
    } else if (code === DisconnectReason.timedOut) { 
    skmod.sendMessage(m.chat, {text : "*❗ La conexión se agotó, se intentara reconectar automáticamente...*"}, { quoted: m }) 
    await reloadHandler(true).catch(console.error)
@@ -172,7 +171,16 @@ let handler = require('./handler.js')
   conn.ev.off('creds.update', conn.credsUpdate)
   conn.ev.off('messages.update', conn.pollCmd)
   }
-
+  
+  conn.ev.on('messages.upsert', conn.connection)
+  conn.ev.on('call', conn.onCall)
+  conn.ev.on('group-participants.update', conn.participantsUpdate)
+  conn.ev.on("groups.update", conn.groupsUpdate)
+  conn.ev.on('message.delete', conn.deleteUpdate)
+  conn.ev.on('connection.update', conn.connectionUpdate)
+  conn.ev.on('creds.update', conn.credsUpdate)
+  conn.ev.on('messages.update', conn.pollCmd)
+  
   conn.connection = handler.handler.bind(conn)
   conn.participantsUpdate = handler.participantsUpdate.bind(conn)
   conn.groupsUpdate = handler.groupsUpdate.bind(conn)
@@ -184,14 +192,7 @@ let handler = require('./handler.js')
   
 
   
-  conn.ev.on('messages.upsert', conn.connection)
-  conn.ev.on('call', conn.onCall)
-  conn.ev.on('group-participants.update', conn.participantsUpdate)
-  conn.ev.on("groups.update", conn.groupsUpdate)
-  conn.ev.on('message.delete', conn.deleteUpdate)
-  conn.ev.on('connection.update', conn.connectionUpdate)
-  conn.ev.on('creds.update', conn.credsUpdate)
-  conn.ev.on('messages.update', conn.pollCmd)
+  
   isInit = false
   return true
   }
