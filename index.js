fcfbb57cd4d9c76925e187be03f8fd6c5a04f49e
skid <112231743+Skidy89@users.noreply.github.com>
2023-10-14 07:31:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ if (connection == 'open') {
 conn.logger.info(`\n╭┈ ┈ ┈ ┈ ┈ • ${vs} • ┈ ┈ ┈ ┈ ┈╮\n┊Skid bot Se Conecto Correctamente a WhatsApp\n╰┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈╯`)
 fs.readdirSync(__dirname + "./commands").forEach((plugin) => {
 if (path.extname(plugin).toLowerCase() == ".js") {
-require(__dirname + "./commands/" + plugin);
+require(__dirname + "/commands/" + plugin);
 }
 })
 }})
