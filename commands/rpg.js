2e5a5f78e40c001bb7b5e5edea155eff35c1c2f0
skid <112231743+Skidy89@users.noreply.github.com>
2023-10-14 18:28:48 +0200

    Add files via upload

--- commands/rpg.js
+++ commands/rpg.js
@@ -1,12 +1,12 @@
-const skid = require('../lib/commands.js')
+const { cmd, msToTime, pickRandom }= require('../lib/commands.js')
 
-skid.cmd({
+cmd({
 pattern: "lb",
 desc: "muestra la leadboard",
 category: "rpg",
 filename: __filename,
 },
-async (conn, m, text, { participants }) => {
+async (conn, m, { participants }) => {
 var body = (typeof m.text == 'string' ? m.text : '') 
 const args = body.trim().split(/ +/).slice(1)  || []
  let member = participants.map(u => u.id) 
@@ -54,6 +54,159 @@ const args = body.trim().split(/ +/).slice(1)  || []
  })
 
 
+cmd({
+pattern: "aventura",
+desc: "aventurate por recompensas",
+category: "rpg",
+filename: __filename,
+},
+async (conn, m) => {
+  let cooldown = 10000
+  let user = global.db.data.users[m.sender]
+  let timer = (cooldown - (new Date - user.lastadventure))
+  if (new Date() - user.lastadventure < 10000) throw `*estas demasiado cansado*\n*espera ${msToTime(cooldown - new Date())} para volver a aventurar*`
+  if (user.health < 80) return conn.reply(m.chat, `*estas herido*\npara poder aventurar necesitas minimo 80 de *salud* ♥️\ncompra pociones con ${global.prefix}buy potion y curate con ${global.prefix}health`, m)
+  let rewards = reward(user)
+  let txt = '*fuiste a una aventura peligrosa*\n*donde perdiste*'
+  for (let lost in rewards.lost) {
+  let total= rewards.lost[lost].getRandom()
+  user[lost] -= total * 1
+  if (total) txt += `\n*${global.rpg.emoticon(lost)}:* ${total}`
+  }
+  txt += '\n\nPero consigues'
+  for (let rewardItem in rewards.reward) {
+  let total = rewards.reward[rewardItem].getRandom()
+  user[rewardItem] += total * 1
+  if (total) txt += `\n*${global.rpg.emoticon(rewardItem)}:* ${total}`
+  }
+  m.reply(txt.trim())
+  user.lastadventure = new Date() * 1
+  
+  function reward(user = {}) { 
+     let rewards = { 
+         reward: { 
+             money: 201 + user.dog * 2000,
+             exp: 301 + user.dog * 2000,
+             trash: 101, 
+             potion: 2, 
+             rock: 2, 
+             wood: 2, 
+             string: 2, 
+             common: 2 * (user.dog && (user.dog > 2 ? 2 : user.dog) * 1.2 || 1), 
+             uncommon: [0, 0, 0, 1, 0].concat( 
+                 new Array(5 - ( 
+                     (user.dog > 2 && user.dog < 6 && user.dog) || (user.dog > 5 && 5) || 2 
+                 )).fill(0) 
+             ), 
+             mythic: [0, 0, 0, 0, 0, 1, 0, 0, 0].concat( 
+                 new Array(8 - ( 
+                     (user.dog > 5 && user.dog < 8 && user.dog) || (user.dog > 7 && 8) || 3 
+                 )).fill(0) 
+             ), 
+             legendary: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0].concat( 
+                 new Array(10 - ( 
+                     (user.dog > 8 && user.dog) || 4 
+                 )).fill(0) 
+             ), 
+             iron: [0, 0, 0, 1, 0, 0], 
+             gold: [0, 0, 0, 0, 0, 1, 0], 
+             diamond: [0, 0, 0, 0, 0, 0, 1, 0].concat( 
+                 new Array(5 - ( 
+                     (user.fox < 6 && user.fox) || (user.fox > 5 && 5) || 0 
+                 )).fill(0) 
+             ), 
+         }, 
+         lost: { 
+             health: 101 - user.cat * 4 
+         } 
+     } 
+     return rewards 
+ }
+})
+cmd({
+pattern: "craft",
+desc: "craftea varias herramientas con este comando",
+category: 'rpg',
+},
+async (conn, m) => {
+let repairs =  (args[0] || '').toLowerCase()
+let user = global.db.data.users[m.sender] 
+let caption = `
+*Por alguna razon tienes estas recetas*
+*(por un momento piensas crear una...)*
+ 
+ *❏ Recetas*
+
+*talvez un trabajo pida un hacha... nunca se sabe*
+ ▧ Hacha 🪓
+ 〉4 madera
+ 〉3 hierro
+ 
+*una buena decisión para conseguir materiales*
+ ▧ Pico ⛏️ 
+ 〉 10 roca
+ 〉 5 Hierro 
+ 〉 2 madera
+
+*necesitas pelear? esta es tu opcion*
+ ▧ espada ⚔️ 
+ 〉 10 madera
+ 〉 15 hierro
+ 
+*un poco de protección nunca viene mal*
+ ▧ Armadura 🥼 
+ 〉 30 diamantes
+ `
+ switch (repairs) {
+
+ case 'hacha': {
+ if (user.axe > 0) m.reply(`*te sientes estupido al intentar crear una hacha cuando ya tienes una...*\n(talvez querías mejorarlo con ${global.prefix}mejorar)`)
+ if (user.wood < 4 || user.iron < 3)  m.reply(`*Te das cuenta que te faltan materiales...*\n(puedes intentar checar tu inventario con .inv)`)
+ user.wood -= 4
+ user.iron -= 3
+ user.axe += 1
+ user.axedurability = 70
+ m.reply('*solo te cortaste una mano para tenér una fabulosa hacha 🪓*')
+ }
+ break
+ 
+ case 'pico': {
+ if (user.pickaxe > 0) throw `*te sientes estupido al intentar crear un pico cuando ya tienes uno...*\n(talvez querías mejorarlo con ${global.prefix}mejorar)`
+ if (user.rock < 10 || user.iron < 5 || user.wood < 2) throw `*Te das cuenta que te faltan materiales...*\n(puedes intentar checar tu inventario con .inv)`
+ user.rock -= 10
+ user.iron -= 5
+ user.wood -= 2
+ user.pickaxe += 1
+ user.pickaxedurability = 70
+ m.reply('*crafteaste un pico ⚒️*')
+ }
+ break
+ 
+ case 'espada': {
+ if (user.pickaxe > 0) throw `*te sientes estupido al intentar crear una espada cuando ya tienes una...*\n(talvez querías mejorarlo con ${global.prefix}mejorar)`
+ if (user.wood < 10 || user.iron < 15) throw `*Te das cuenta que te faltan materiales...*\n(puedes intentar checar tu inventario con .inv)`
+ user.wood -= 10
+ user.iron -= 15
+ user.sword += 1
+ user.sworddurability = 70
+ m.reply('*con unas cuantas lesiones y cortaduras creaste una espada ⚔️*')
+ }
+ break
+ 
+ case 'armadura': {
+ if (user.armor > 0) throw `*te sientes estupido al intentar crear una armadura cuando ya tienes una...*\n(talvez querías mejorarlo con ${global.prefix}mejorar)`
+ if (user.diamond < 30) throw `*Te das cuenta que te faltan materiales...*\n(puedes intentar checar tu inventario con .inv)`
+ user.diamond -= 30
+ user.armor += 1
+ user.armordurability = 70
+ m.reply('*como diablos hiciste una armadura con diaman.. da igual, lo bueno es que tienes ahora una armadura*')
+ }
+ break
+ 
+ default: 
+ m.reply(caption)
+ }
+ })
 
  function sort(property, ascending = true) { 
    if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]; 
